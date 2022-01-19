daa3e8928ce900501b5b1a96df612bc87606c6d2
awesomedana <dana042118@gmail.com>
2022-01-19 21:04:07 +0900

    Indeed 채용글 스크랩 완료

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
 from indeed import extract_indeed_pages, extract_indeed_jobs
 
+os.system("clear")
 
 last_indeed_page = extract_indeed_pages()
-jobs = extract_indeed_jobs(last_indeed_page)
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+print(indeed_jobs)
