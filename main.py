a46c55a003da65645360388333917df9406c1930
awesomedana <dana042118@gmail.com>
2022-01-19 21:27:07 +0900

    강의 2.9까지.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 from indeed import get_jobs as get_indeed_jobs
+from stackoverflow import get_jobs as get_so_jobs
 
 os.system("clear")
 
-indeed_jobs = get_indeed_jobs()
-print(indeed_jobs)
+# indeed_jobs = get_indeed_jobs()
+# print(indeed_jobs)
+
+so_jobs = get_so_jobs()
+print(so_jobs)
