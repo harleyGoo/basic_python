bd82ab587da888092b84fedc7726853025bdf2ed
awesomedana <dana042118@gmail.com>
2022-01-20 21:15:01 +0900

    Indeed, StackOverflow 채용글 CSV파일로 출력(~2.16강의)

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 from indeed import get_jobs as get_indeed_jobs
 from stackoverflow import get_jobs as get_so_jobs
+from save import save_to_file
 
 os.system("clear")
 
-# indeed_jobs = get_indeed_jobs()
+indeed_jobs = get_indeed_jobs()
 # print(indeed_jobs)
 
 so_jobs = get_so_jobs()
-print(so_jobs)
+# print(so_jobs)
+
+jobs = indeed_jobs + so_jobs
+save_to_file(jobs)
+
+# CSV: Comma Seperated Values
